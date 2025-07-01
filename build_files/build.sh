77e29c924ad1ba68b24613afece76aca794a025e
Cole Neville <5910739+ColeNeville@users.noreply.github.com>
2025-07-01 15:40:45 -0600

    feat: install a couple things into server-os

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,22 +2,17 @@
 
 set -ouex pipefail
 
-### Install packages
+dnf5 remove -y \
+  docker \
+  docker-compose
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+dnf5 install -y \
+  podman-compose \
+  podman-docker
 
-# this installs a package from fedora repos
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
+dnf5 config-manager addrepo \
+  --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo
+dnf5 install -y tailscale
 
 systemctl enable podman.socket
+systemctl enable tailscale.socket
